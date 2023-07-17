3b3ec4137ae8b221609ccf1edb5941b7043f6802
Hiroki-kirihara <136045081+Hiroki-kirihara@users.noreply.github.com>
2023-07-17 17:51:20 +0800

    migrate.model.created

---
+++ database/migrations/2023_07_17_041522_create_reservations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            // $table->unsignedBigInteger('restaurants_id');
+            $table->unsignedBigInteger('customers_id');
+            // $table->foreign('restaurants_id')->references('id')->on('restaurants');
+            $table->foreign('customers_id')->references('id')->on('customers');
+            $table->date('date');
+            $table->time('time');
+            $table->integer('people');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
