70b22d8ffa91afd53acd16ad9d38142230cf3fb4
koheyyoshide <136044961+koheyyoshide@users.noreply.github.com>
2023-07-18 15:50:25 +0800

    restaurant login/register auth created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/login/restaurant', [App\Http\Controllers\Auth\LoginController::class, 'showRestaurantLoginForm']);
+Route::get('/register/restaurant', [App\Http\Controllers\Auth\RegisterController::class, 'showRestaurantRegisterForm']);
+
+Route::post('/login/restaurant', [App\Http\Controllers\Auth\LoginController::class, 'restaurantLogin']);
+Route::post('/register/restaurant', [App\Http\Controllers\Auth\RegisterController::class, 'registerRestaurant'])->name('restaurant-register');
+
+Route::view('/restaurant', 'restaurant')->middleware('auth:restaurant')->name('restaurant-home');
+Route::get('/login/restaurant', [RestaurantController::class, 'showLoginForm'])->name('login.restaurant');
+Route::get('/register/restaurant', [RestaurantController::class, 'showRegistrationForm'])->name('register.restaurant');
+
+
