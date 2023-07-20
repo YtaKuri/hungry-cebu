3391cf4a7384a52f26da439c1202d7466c0e6884
Hiroki-kirihara <136045081+Hiroki-kirihara@users.noreply.github.com>
2023-07-20 10:08:58 +0800

    routing.2

---
+++ app/Http/Controllers/ReserveController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReserveController extends Controller
+{
+    //
+    public function create()
+    {
+        return view('posts.reserve');
+    }
+
+}
