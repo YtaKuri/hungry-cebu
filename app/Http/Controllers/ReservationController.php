3a495ce688d1b8debe440fab8095e972cc622c2d
YtaKuri <132805023+YtaKuri@users.noreply.github.com>
2023-07-29 15:49:28 +0800

    initial.commit

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -7,22 +7,6 @@ use App\Models\Reservation;
 
 class ReservationController extends Controller
 {
-    public function index()
-    {
-        // 複数の予約情報を取得
-        $admins = Admin::all();
-
-        return view('posts.index', ['admins' => $admins]);
-    }
-
-        public function show($id)
-    {
-        // 特定の予約情報を取得
-        $admin = Admin::find($id);
-
-        return view('posts.show', ['admin' => $admin]);
-    }
-   
     // 予約作成ページを表示する
     public function create()
     {
