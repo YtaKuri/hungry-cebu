eb654d693229da96dc49074785166b5918b054f4
Hiroki-kirihara <136045081+Hiroki-kirihara@users.noreply.github.com>
2023-07-30 19:55:30 +0800

    security1

---
+++ app/Http/Middleware/AuthenticateAdmin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticateAdmin
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()) {
+            // ユーザーがログインしている場合は、リクエストを次のミドルウェアに進める
+            return $next($request);
+        } else {
+            // ユーザーがログインしていない場合は、ログインページにリダイレクト
+            return redirect('admin/login');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\Auth\AdminAuthController;
 use App\Http\Controllers\Auth\AdminRegisterController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ReservationController;
-
+use App\Http\Middleware\AuthenticateUser;
+use App\Http\Middleware\AuthenticateAdmin;
 
 /*
 |--------------------------------------------------------------------------
@@ -75,3 +76,55 @@ Route::delete('/reservations/{id}', [ReservationController::class, 'destroy'])->
 Route::get('/your-route', 'YourController@yourMethod')->middleware('auth:admin');
 
 Route::post('/admin/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+
+Route::get('/dashboard', function () {
+    // このルートはログインしていないとアクセスできない
+})->middleware('auth');
+
+Route::middleware([AuthenticateUser::class])->group(function () {
+
+    Route::get('/index', 'App\Http\Controllers\IndexController@index')->name('posts.index');
+
+    Route::get('/index/{id}', 'App\Http\Controllers\IndexController@show')->name('posts.show');
+    
+    Route::get('/reserve', 'App\Http\Controllers\ReserveController@create')->name('posts.create');
+    
+    Route::get('/reserve/{id}', 'App\Http\Controllers\ReserveController@show')->name('posts.show');
+    
+    Route::post('/reserve', 'App\Http\Controllers\ReserveController@store')->name('posts.store');
+    
+    Route::get('/mypage', 'App\Http\Controllers\MypageController@show')->name('posts.mypage');
+    
+    Route::get('/mypage/{id}/edit','App\Http\Controllers\MypageController@edit')->name('posts.edit');
+    
+    Route::put('/mypage/{id}', 'App\Http\Controllers\MypageController@update')->name('posts.update');
+    
+    Route::delete('/mypage/{id}','App\Http\Controllers\MypageController@destroy')->name('posts.destroy');
+    
+    
+});
+
+
+Route::middleware([AuthenticateAdmin::class])->group(function () {
+
+    // 予約一覧ページ
+    Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
+
+    // 予約追加ページ
+    Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
+
+    // 予約保存
+    Route::post('/reservations', [ReservationController::class, 'store'])->name('reservations.store');
+
+    // 予約詳細ページ
+    Route::get('/reservations/id/{id}', [ReservationController::class, 'show'])->name('reservations.show');
+
+    // 予約削除
+    Route::delete('/reservations/{id}', [ReservationController::class, 'destroy'])->name('reservations.destroy');
+
+    Route::get('/your-route', 'YourController@yourMethod')->middleware('auth:admin');
+
+    Route::post('/admin/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+
+
+});
